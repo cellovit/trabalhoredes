98292c19a0a3fa43dc95a32fcd4fe27329cef40e
cellovit <marcellov1t@hotmail.com>
2016-06-16 15:11:33 -0300

    estruturação do envio de pacotes com numeros de sequencia

--- trabalhoredes/src/Cliente.java
+++ trabalhoredes/src/Cliente.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.util.Scanner;
         
 
@@ -7,6 +8,7 @@ public class Cliente {
     
     static final int portaServidor = 8002;
     static final int tamanhoPacote = 1000;
+    int nSeqAtual;
     
     public Cliente(){}
     
@@ -16,20 +18,40 @@ public class Cliente {
         FileInputStream fis = new FileInputStream(f);
         int tamanhoArquivo = fis.available();
         
+        //if (tamanhoArquivo != 0){
         System.out.println("Nome do arquivo : " + f.getName());
         System.out.println("Tamanho arquivo : " + f.length());
         
         byte[] sendData = new byte[tamanhoPacote];
-        for (int i = 0; i < f.length(); ){
+        for (int i = 0; i < tamanhoArquivo; i++ ){
             sendData[i] = (byte)fis.read();
         }
         return sendData;
     }
     
+    //método que constroi um unico pacote de determinado numero de sequencia com o cabeçalho + dados
+    public byte[] preparaPacote(int nSeq, byte[] sendData){
+        
+        ByteBuffer bufferPacote = ByteBuffer.allocate(4 + tamanhoPacote); //4 bytes pro numero de sequencia + tamanho do pacote
+        bufferPacote.putInt(nSeq);  //insere o numero de sequencia no buffer
+        bufferPacote.put(sendData[nSeq]); //insere dados no buffer
+        return bufferPacote.array();
+    }
+
+    public int getnSeqAtual() {
+        return nSeqAtual;
+    }
+
+    public void setnSeqAtual(int nSeqAtual) {
+        this.nSeqAtual = nSeqAtual;
+    }
+    
     public void enviaPacote (byte[] pacote, InetAddress IPAddress, int nSeq) throws SocketException, IOException{
+        
         
         DatagramPacket p = new DatagramPacket(pacote, pacote.length, IPAddress, portaServidor);
         DatagramSocket s = new DatagramSocket();
+        //s.setSoTimeout(300);
         s.send(p);
     }
 }
--- trabalhoredes/src/Interface.java
+++ trabalhoredes/src/Interface.java
@@ -1,4 +1,7 @@
 
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
@@ -14,25 +17,37 @@ import java.util.Scanner;
  */
 public class Interface {
     
+    static final int portaServidor = 8002;
+    static final int tamanhoPacote = 1000;
+    
+    
     public static void main(String args[]) throws Exception {
         
         boolean transferenciaCompleta = false;
+        int ssthresh = 16;
+        int tamanhoJanela = 1;
         
         Cliente cliente = new Cliente();
+        int nSeqAtualCliente = cliente.getnSeqAtual();
         
         Scanner input = new Scanner (System.in);
-        String caminhoArquivo = input.nextLine();
-        
         // Resolução de nome de host.
         InetAddress IPAddress = InetAddress.getByName("hostname");
         
         System.out.println("Insira o caminho do arquivo que deseja enviar");
-        String caminho = input.nextLine();
+        String caminhoArquivo = input.nextLine();
         
-        cliente.preparaArquivo(caminhoArquivo);
+        byte[] bufferArquivo = cliente.preparaArquivo(caminhoArquivo);
         
         while(!transferenciaCompleta){
-            
+            for (int i = cliente.getnSeqAtual(); i < tamanhoJanela; i++){
+                System.out.println("Enviando pacote com numero de sequencia : " + nSeqAtualCliente);
+                cliente.enviaPacote(cliente.preparaPacote(nSeqAtualCliente, bufferArquivo), IPAddress, i);
+            }
+            if (tamanhoJanela < ssthresh){
+                tamanhoJanela *= 2;
+            }
+            nSeqAtualCliente++;
         }
         
         
